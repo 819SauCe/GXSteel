62a7edc3d574f33cee2a4d9e4781d5bbc537cde1
819SauCe <joaovitor819oqueres@gmail.com>
2025-07-29 15:25:39 -0300

    fix

--- GXSteel Front-end/src/routes/produto/+page.svelte
+++ GXSteel Front-end/src/routes/produto/+page.svelte
@@ -1,9 +1,3 @@
 <svelte:head>
   <meta http-equiv="refresh" content="0;url=/catalogo" />
 </svelte:head>
-
-<script>
-  import Loading from '../../components/Loading.svelte'
-</script>
-
-<Loading />
